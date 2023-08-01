376fb3f41d7e8a5dea73590b68ef12b6e7c378dd
Jacob Pfundstein <jjpfst@gmail.com>
2023-08-01 19:10:57 +0200

    Initial commit

---
+++ TennisRankings/Data/ATPRanking.cs
@@ -0,0 +1,8 @@
+﻿namespace TennisRankings;
+
+public class ATPRanking
+{
+    public string Name { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public int Points { get; set; }
+}
---
+++ TennisRankings/Data/ATPRankingService.cs
@@ -0,0 +1,14 @@
+﻿namespace TennisRankings;
+
+public class ATPRankingService
+{
+    public Task<ATPRanking[]> GetRankingsAsync()
+    {
+        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new ATPRanking
+        {
+            Name = "Player " + index,
+            Country = "Country " + index,
+            Points = Random.Shared.Next(1000, 2000)
+        }).ToArray());
+    }
+}
