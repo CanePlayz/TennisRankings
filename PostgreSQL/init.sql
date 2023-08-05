f3e278c68a1a1cb1b66cbac8f299a31711a9cb28
Jacob Pfundstein <jjpfst@gmail.com>
2023-08-05 15:47:22 +0200

    Minor fixes

--- PostgreSQL/init.sql
+++ PostgreSQL/init.sql
@@ -1,13 +1,2 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-DO
-$do$
-BEGIN
-   IF NOT EXISTS (
-      SELECT FROM pg_catalog.pg_database
-      WHERE datname = 'rankings') THEN
-
-      CREATE DATABASE rankings;
-   END IF;
-END
-$do$;
+CREATE DATABASE rankings;
