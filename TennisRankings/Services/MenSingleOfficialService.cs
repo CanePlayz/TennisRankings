35de420044b707a57b974c00410cb6a274cb3cb3
Jacob P. <jjpfst@gmail.com>
2023-10-25 01:56:33 +0200

    Add environment variable to fix API requests

--- TennisRankings/Services/MenSingleOfficialService.cs
+++ TennisRankings/Services/MenSingleOfficialService.cs
@@ -13,7 +13,23 @@ public class MenSingleOfficialService
 
     public async Task<List<TennisRankingOfficial>> GetRankingsAsync()
     {
-        var result = await _httpClient.GetFromJsonAsync<List<TennisRankingOfficial>>("http://blue-api/mensingleofficial");
+        string activeEnvironment = Environment.GetEnvironmentVariable("ACTIVE_ENVIRONMENT") ?? "blue";
+        string apiUrl;
+
+        if (activeEnvironment == "blue")
+        {
+            apiUrl = "http://blue-api/mensingleofficial";
+        }
+        else if (activeEnvironment == "green")
+        {
+            apiUrl = "http://green-api/mensingleofficial";
+        }
+        else
+        {
+            throw new Exception("Invalid environment configuration.");
+        }
+
+        var result = await _httpClient.GetFromJsonAsync<List<TennisRankingOfficial>>(apiUrl);
         return result ?? new List<TennisRankingOfficial>();
     }
 }
--- scripts/switch_environments.bash
+++ scripts/switch_environments.bash
@@ -1,11 +1,8 @@
 # Check if blue is running
 BLUE_RUNNING=$(docker-compose -f docker-compose_blue.yaml ps | grep Up)
 
-ACTIVE_COMPOSE=""
-INACTIVE_COMPOSE=""
-
 # Determine which environment is active
-if [ -n "$BLUE_RUNNING" ]; then
+  if [ -n "$BLUE_RUNNING" ]; then
     ACTIVE_COMPOSE="docker-compose_blue.yaml"
     INACTIVE_COMPOSE="docker-compose_green.yaml"
 else
@@ -21,11 +18,21 @@ docker-compose -f $INACTIVE_COMPOSE up -d
 
 # Update nginx configuration based on the environment
 if [ "$INACTIVE_COMPOSE" == "docker-compose_green.yaml" ]; then
-    sed -i '/# BEGIN DEFAULT UPSTREAM/,/# END DEFAULT UPSTREAM/s|proxy_pass http://blue_upstream;|proxy_pass http://green_upstream;|' /etc/nginx/conf.d/tennisrankings.conf
-    sed -i '/# BEGIN DEFAULT UPSTREAM/,/# END DEFAULT UPSTREAM/s|proxy_pass http://green_upstream;|proxy_pass http://blue_upstream;|' /etc/nginx/conf.d/tennisrankings.conf
+    sed -i ' \
+        /# BEGIN DEFAULT UPSTREAM/,/# END DEFAULT UPSTREAM/ { \
+            s| \
+            proxy_pass http://blue_upstream;| \
+            proxy_pass http://green_upstream;| \
+        }' /etc/nginx/conf.d/tennisrankings.conf
+    sed -i ' \
+        /# BEGIN DEFAULT UPSTREAM/,/# END DEFAULT UPSTREAM/ { \
+            s| \
+            proxy_pass http://gree_upstream;| \
+            proxy_pass http://blue_upstream;| \
+        }' /etc/nginx/conf.d/tennisrankings.conf
 fi
 
-# Reload Nginx Configuration
+# Reload nginx Configuration
 nginx -s reload
 
 # Stop the old environment
