53e975e08acfad0c59bc76d453da32835ad7fbd2
Jacob Pfundstein <jjpfst@gmail.com>
2023-11-14 23:38:50 +0100

    Update to .NET 8 and account for new default port

--- TennisRankings/Services/MenSingleOfficialService.cs
+++ TennisRankings/Services/MenSingleOfficialService.cs
@@ -14,7 +14,7 @@ public class MenSingleOfficialService
     public async Task<List<TennisRankingOfficial>> GetRankingsAsync()
     {
         string activeEnvironment = Environment.GetEnvironmentVariable("ACTIVE_ENVIRONMENT") ?? "blue";
-        string apiUrl = $"http://{activeEnvironment}-api/mensingleofficial";
+        string apiUrl = $"http://{activeEnvironment}-api:8080/mensingleofficial";
 
         var result = await _httpClient.GetFromJsonAsync<List<TennisRankingOfficial>>(apiUrl);
         return result ?? new List<TennisRankingOfficial>();
